c3045340558b7def37b9a8dce9fd6cffb0135007
Jordan Jones <jordanjones0902@gmail.com>
2024-12-11 22:28:36 -0500

    Starting Solidity Course

---
+++ Learn_Solidity/ExampleContrct.sol
@@ -0,0 +1,12 @@
+// Smart Contract example
+
+contract Agreement {
+  address recipient;
+  bool conditionIsmet;
+
+  function payout () external {
+    if(conditionIsmet) {
+      sendValue(recipient);
+    }
+  }
+}
